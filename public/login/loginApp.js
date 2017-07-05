d12c2f7aeb30c6d8de8aed50eb488912989146a4
puzant <PBAKJEJIAN@hotmail.com>
2017-07-05 18:25:58 +0300

    upload new changes

---
+++ public/login/loginApp.js
@@ -0,0 +1,11 @@
+var app = angular.module("app", []);
+
+app.controller('ctrl', function($scope) {
+    $scope.login = function() {
+//        console.log($scope.username);
+//        console.log($scope.password);
+        
+        
+        
+    }
+})
--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-var express = require('express');
+var express = require("express");  
+var app = express();  
+var path = require("path");  
+var morgan = require("morgan");  
+var mongoose = require("mongoose");  
 var bodyParser = require("body-parser");
-var app = express();
-var path = require("path");
-var mongoose = require("mongoose");
-// mongoose.Promise = require('bluebird');
-var port = process.env.PORT || 8000;
+var expressJwt = require("express-jwt");
 
 
+var port = process.env.PORT || 5000;
+
+app.use(morgan("dev"));  
 app.use(bodyParser.json());
 
+//app.use(express.static(path.join(__dirname, "public")));
+app.use('/auth', require('./routes/authroutes'));
 
-app.use(express.static(path.join(__dirname, "public")));
+mongoose.connect("mongodb://localhost/USER", function (err) {  
+    if (err) throw err;
+    console.log("Successfully connected to the database");
+});
 
+app.get("/", function (req, res) {  
+    res.send("It's working");
+});
 
-
-app.listen(port, function() {
-  console.log(`listening on ${port}`)
+app.listen(port, function () {  
+    console.log(`Server listening on port ${port}`);
 });
